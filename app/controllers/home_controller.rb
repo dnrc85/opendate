469c58891377c4de59005942fb5655fa900041e1
Rob Williams <robdwilliams85@gmail.com>
2015-09-10 22:00:10 +0000

    Refactor to work with pgres

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
     
     def index
         @treatments = Treatment.all.reorder("completed_at")
-        @treatments_outstanding = Treatment.all.reorder("date_due_back")
+        @treatments_outstanding = Treatment.where(completed_at: nil).order("date_due_back")
         @orders = Order.all.reorder("completed_at")
         @orders_outstanding = Order.where(completed_at: nil).where.not(due_date: nil).order("due_date")
     end
