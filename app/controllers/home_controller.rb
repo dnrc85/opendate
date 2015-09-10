393ce028dc33eaa97166485747a7a484b4fee0ba
Rob Williams <robdwilliams85@gmail.com>
2015-09-10 21:54:26 +0000

    Rework to try to match up with postgres

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,7 +4,7 @@ class HomeController < ApplicationController
         @treatments = Treatment.all.reorder("completed_at")
         @treatments_outstanding = Treatment.all.reorder("date_due_back")
         @orders = Order.all.reorder("completed_at")
-        @orders_outstanding = Order.all.reorder("due_date")
+        @orders_outstanding = Order.where(completed_at: nil).where.not(due_date: nil).order("due_date")
     end
     
 end
